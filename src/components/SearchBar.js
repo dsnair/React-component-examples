9a2fb2a5c83437e718090986d2862cb92f4228d0
Divya Nair <divnairs@gmail.com>
2019-03-26 12:48:07 -0700

    create and style searchbar responsively

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import './SearchBar.css'
 
 const SearchBar = () => {
   return (
-    "This is a search bar"
+    <div className="searchbar">
+      <div className="instagram">
+        <i className="fab fa-instagram" />
+        <span> Instagram</span>
+      </div>
+      <form>
+        <i className="fas fa-search" />
+        <input type="search" placeholder="Search" height="100"/>
+      </form>
+      <div>
+        <i className="far fa-compass" />
+        <i className="far fa-heart" />
+        <i className="far fa-user" />
+      </div>
+    </div>
   )
 }
 export default SearchBar
